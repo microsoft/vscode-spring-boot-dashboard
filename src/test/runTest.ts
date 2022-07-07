edacc41969e73c7631b9cc82e2d21d901d1dd08e
Shi Chen <chenshi@microsoft.com>
2022-07-07 11:02:23 +0800

    install dependency extensions

--- src/test/runTest.ts
+++ src/test/runTest.ts
@@ -2,15 +2,35 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT license.
 
+import * as cp from "child_process";
 import * as path from "path";
 import * as os from "os";
 
-import { downloadAndUnzipVSCode, runTests } from "@vscode/test-electron";
+import { downloadAndUnzipVSCode, resolveCliArgsFromVSCodeExecutablePath, runTests } from "@vscode/test-electron";
 
 async function main(): Promise<void> {
     try {
 
         const vscodeExecutablePath = await downloadAndUnzipVSCode();
+
+        /**
+         * Install dependency extensions
+         */
+        const [cli, ...args] = resolveCliArgsFromVSCodeExecutablePath(vscodeExecutablePath);
+        cp.spawnSync(cli, [...args, '--install-extension', 'pivotal.vscode-spring-boot'], {
+            encoding: 'utf-8',
+            stdio: 'inherit'
+        });
+
+        cp.spawnSync(cli, [...args, '--install-extension', 'redhat.java'], {
+            encoding: 'utf-8',
+            stdio: 'inherit'
+        });
+
+        cp.spawnSync(cli, [...args, '--install-extension', 'vscjava.vscode-java-debug'], {
+            encoding: 'utf-8',
+            stdio: 'inherit'
+        });
 
         // The folder containing the Extension Manifest package.json
         // Passed to `--extensionDevelopmentPath`
