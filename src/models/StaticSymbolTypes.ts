bba3a463b4a73fe903b2131b8448fb281459f258
Yan Zhang <yanzh@microsoft.com>
2022-08-17 15:59:24 +0800

    Revert "use window.openTextDocument(preserveFocus) to keep focus on tree view"
    This reverts commit 46278eaf57189becc3669c63b56957aa35e4e72b.

--- src/models/StaticSymbolTypes.ts
+++ src/models/StaticSymbolTypes.ts
@@ -1,6 +1,3 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT license.
-
 import * as vscode from "vscode";
 
 export abstract class StaticSymbol {
@@ -42,9 +39,9 @@ export class StaticEndpoint extends StaticSymbol {
     // parsed
     public get method(): string | undefined {
         return this.raw.method;
-    }
+    };
 
     public get pattern(): string | undefined {
         return this.raw.pattern;
-    }
+    };
 }
