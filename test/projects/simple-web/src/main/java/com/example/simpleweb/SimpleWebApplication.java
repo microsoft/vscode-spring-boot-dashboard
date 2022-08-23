beb68a3f31dda0dd7b912b5db78da49b12ffea70
Yan Zhang <Eskibear@users.noreply.github.com>
2022-08-23 10:42:49 +0800

    show correct status when actuator is absent (#205)

---
+++ test/projects/simple-web/src/main/java/com/example/simpleweb/SimpleWebApplication.java
@@ -0,0 +1,13 @@
+package com.example.simpleweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleWebApplication.class, args);
+	}
+
+}
