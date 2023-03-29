d52b989fb081e319de38338380ad1acd92a03cbf
Yan Zhang <Eskibear@users.noreply.github.com>
2023-03-29 14:34:54 +0800

    Update build-java-ext.js

--- scripts/build-java-ext.js
+++ scripts/build-java-ext.js
@@ -10,7 +10,7 @@ const cp = require('child_process');
 const path = require('path');
 const serverDir = path.join(__dirname, '..', 'java-extension');
 
-cp.execSync(`${mvnw()} clean package`, { cwd: serverDir, stdio: [0, 1, 2] });
+cp.execSync(`${mvnw()} clean package -B`, { cwd: serverDir, stdio: [0, 1, 2] });
 const fs = require('fs');
 const libDir = path.join(__dirname, "..", "lib");
 if (!fs.existsSync(libDir)) {
